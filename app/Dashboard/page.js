b175b5ac87c11ce4b8a7deac51a0b7399f98d3df
abeerfedo <adamabeer44@gmail.com>
2026-02-13 21:37:08 +0300

    make sure database and website is synced/connected

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -3,11 +3,13 @@ import LogoutButton from "@/components/LogoutButton";
 
 export default async function Dashboard() {
     const session = await auth();
+    const userPoints = session?.user?.points ?? 0;
     if (!session) {
   return <p>Redirecting...</p>; // Or just return null
 }
     return <>
         <h1>hi welcome back,{session?.user?.name}  </h1>
+        <h1>you have {session?.user?.points} number of points </h1>
         <LogoutButton/ >
 
     </>
--- auth.js
+++ auth.js
@@ -3,6 +3,9 @@ import GoogleProvider from 'next-auth/providers/google'
 import { SupabaseAdapter } from "@auth/supabase-adapter";
 
 export const  { handlers, signIn, signOut, auth} = NextAuth ({
+
+  // here the code to connect or sync with supabase profile table 
+
     providers : [ 
             GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -14,16 +17,16 @@ export const  { handlers, signIn, signOut, auth} = NextAuth ({
   }),
 events: {
     async signIn({ user }) {
-      // This is where you could manually link sessions if needed,
-      // but for now, let's keep it simple. 
+    
       console.log("👤 User signed in via Auth.js:", user.email)
     },
   },
   callbacks: {
     async session({ session, user }) {
-      // This makes the User ID available in the session object
+
       if (session.user) {
         session.user.id = user.id;
+        session.user.points= user.points
       }
       return session;
     }
